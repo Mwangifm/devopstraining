36b54376b1fd7ac2affa34f9c5bd6e756570c83d
mwangifm <Francis.mwangi@strathmore.edu>
2023-06-03 20:44:25 +0300

    new files and amends

--- hello.py
+++ hello.py
@@ -1,4 +1,7 @@
 print("Hello World!!!")
-print(input("What is your Firstname? "))
-print(input("What is your Secondname? "))
+#print(input("What is your Firstname? "))
+#print(input("What is your Secondname? "))
 
+print("O------")
+print("  IIII")
+print("0"*10)
---
+++ strings2.py
@@ -0,0 +1,20 @@
+course = "Python's Course for Beginners"
+print(course)
+
+course2 = 'Python for "Beginners"'
+print(course2[0:3])
+print(course2[0])
+print(course2[-3])
+print(course2[0:])
+print(course2[1:])
+print(course2[:3])
+print(course2[:])
+print(course2[2:-4])
+
+Email = """
+Hi Francis,
+This is the first email from python.
+Thanks you.
+
+"""
+print(Email)
