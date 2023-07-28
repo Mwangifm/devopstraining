d3aba35e3ab208c41a7a147ae024791474dc6bb2
mwangifm <Francis.mwangi@strathmore.edu>
2023-07-28 10:24:57 +0300

    Amend cicd description

---
+++ test_factorial-2.py
@@ -0,0 +1,18 @@
+from factorial import * 
+
+class TestClass: 
+
+    def test_1(self): 
+
+        assert factorial(0) == 1 
+
+    def test_2(self): 
+
+        assert factorial(1) == 1  
+
+    def test_3(self): 
+
+        assert sum_factorial() == 34 
+
+        assert len(final_list) == 5
+
